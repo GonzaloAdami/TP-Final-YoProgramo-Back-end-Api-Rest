6fb4ab1acadbd3f7c65fe1ee7e07666f220f6fe6
Gonzalo <gonzaloadamie@gmail.com>
2023-05-17 03:32:05 -0300

    4.3V

---
+++ src/main/java/com/example/demo/Controller/PersonController.java
@@ -0,0 +1,51 @@
+package com.example.demo.Controlller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Person;
+import com.example.demo.repository.PersonRepository;
+
+@RestController
+@RequestMapping("/api")
+public class PersonController {
+	
+	@Autowired
+	private PersonRepository repository;
+	
+	
+	@GetMapping("/persons")
+	public List<Person> allPersons(){
+		return repository.findAll();
+	}
+	
+	@GetMapping("/person/{name}")
+	public List<Person> findByName(@PathVariable("name") String name) {
+		return repository.findByName(name);
+	}
+	
+	@PostMapping("/person")
+	public Person createPerson(@RequestBody Person person) {
+		return repository.save(person);
+	}
+	
+	@PutMapping("/person/{id}")
+	public Person updatePerson(@PathVariable int id ,@RequestBody Person person) {
+		return repository.save(person);
+	}
+	
+	@DeleteMapping("/person/{id}")
+	public void deletePerson(@PathVariable("id") Long id) {
+		repository.deleteById(id);
+	}
+}
+
