429d2a3678b873958b25c7f34be92315765b4ae7
Gonzalo <gonzaloadamie@gmail.com>
2023-05-17 05:30:40 -0300

    5.3V

--- src/main/java/com/example/demo/Controller/PersonController.java
+++ src/main/java/com/example/demo/Controller/PersonController.java
@@ -4,21 +4,28 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
+
 
 import com.example.demo.model.Person;
 import com.example.demo.repository.PersonRepository;
-
+@CrossOrigin(origins = "https://yoprogramo-frontend-2d963.firebaseapp.com")
 @RestController
 @RequestMapping("/api")
 public class PersonController {
 	
+    
 	@Autowired
 	private PersonRepository repository;
 	
