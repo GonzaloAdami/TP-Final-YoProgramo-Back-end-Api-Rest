215a54e3378af5ed6e3b835811cb58c50eecb153
Gonzalo <gonzaloadamie@gmail.com>
2023-05-17 05:56:43 -0300

    5.4V

---
+++ src/main/java/com/example/demo/CorsConfiguration/CorsConfiguration.java
@@ -0,0 +1,16 @@
+package com.example.demo.CorsConfiguration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://yoprogramo-frontend-2d963.firebaseapp.com")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+}
